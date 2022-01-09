57390e7b9bed552d5c7e2769536932d30d5b16bf
patilrushikesh247@gmail.com <58651016+patilrushikesh247@users.noreply.github.com>
2022-01-09 15:17:38 +0530

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild } from '@angular/core';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+
+  name_check : boolean=false;
+  age_check : boolean=false;
+  average_check: boolean=false;
+  headers :any=[];
+  filedownlaod(){
+    console.log(this.name_check, 'Value of checkbox');
+    if(this.name_check ||this.average_check ||this.age_check){
+      this.headers.push("name","age","average");
+
+      //this.data=this.data["name"];
+    }
+    var options = { 
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true, 
+      showTitle: true,
+      //title: 'report data ',
+      useBom: true,
+      headers: this.headers
+    };
+    var original_data=this.data;
+    var data1:any=[];
+    this.headers.forEach(function(elm:any,key:any){
+      console.log(elm);
+      for(let i=0;i<original_data.length;i++){
+        let obj={
+          name:original_data[i].name,
+          age:original_data[i].age,
+          average:original_data[i].average
+
+         };
+        data1.push(obj);
+      }
+    });
+    new  ngxCsv(data1, "report", options);
+  }
+
+  data = [
+  {
+    name: "Test 1",
+    age: 13,
+    average: 8.2,
+    approved: true,
+    description: "using 'Content here, content here' "
+  },
+  {
+    name: 'Test 2',
+    age: 11,
+    average: 8.2,
+    approved: true,
+    description: "using 'Content here, content here' "
+  },
+  {
+    name: 'Test 4',
+    age: 10,
+    average: 8.2,
+    approved: true,
+    description: "using 'Content here, content here' "
+  },
+  ];
+
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyGalleryComponent } from './gallery/my-gallery/my-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyGalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
